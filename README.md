822a910c585db1293e63ce22b13a2d8a43823bdb
Graciela Contreras <gracielaestefani@gmail.com>
2023-07-12 12:17:56 -0400

    Implementación de métodos GET y POST aplicando la técnica TDD(Test Driven Development)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# cashcard
+## Creación de una API REST con Spring Boot
+Se implementan métodos **GET, POST, PUT y DELETE** aplicando la técnica **TDD (Test Driven Development)**
+
+<br>Con TDD se hacen pruebas antes de implementar el código de la aplicación. Es una técnica probada y verdadera <br>
+para ayudar a los desarrolladores de aplicaciones a diseñar software simple pero robusto, además de protegerse <br> contra regresiones de funcionalidad y errores.
---
+++ src/test/java/example/cashcard/CashcardApplicationTests.java
@@ -0,0 +1,67 @@
+package example.cashcard;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import java.net.URI;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CashcardApplicationTests {
+
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	@Test
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		//	Test the id.
+		//This converts the response String into a JSON-aware object with lots of helper methods.
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.id");
+		assertThat(id).isNotNull();
+		assertThat(id).isEqualTo(99);
+
+		//	Test the amount.
+		Double amount = documentContext.read("$.amount");
+		assertThat(amount).isEqualTo(123.45);
+	}
+
+//	Let's write a new test that expects that we ignore Cash Cards that do not have an id of 99. use 1000
+	@Test
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+
+	@Test
+	void shouldCreateANewCashCard() {
+		CashCard newCashCard = new CashCard(null, 250.00); //null porque el id es incremental
+		ResponseEntity<Void> createResponse = restTemplate.postForEntity("/cashcards", newCashCard, Void.class);
+		assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+
+		URI locationOfNewCashCard = createResponse.getHeaders().getLocation();
+		ResponseEntity<String> getResponse = restTemplate.getForEntity(locationOfNewCashCard, String.class);
+		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		// Add assertions
+		DocumentContext documentContext = JsonPath.parse(getResponse.getBody());
+		Number id = documentContext.read("$.id");
+		Double amount = documentContext.read("$.amount");
+
+		assertThat(id).isNotNull();
+		assertThat(amount).isEqualTo(250.00);
+	}
+
+
+}
